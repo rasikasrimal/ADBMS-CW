1b9bfd35407c5cca60a546433312915248fcbba7
rasikasrimal <rasikacooldj620@gmail.com>
2023-10-10 20:51:02 +0530

    Store Locations

--- supermarket management system/storelocation.py
+++ supermarket management system/storelocation.py
@@ -2,54 +2,54 @@ from tkinter import *
 from tkinter import ttk
 
 
-def open_order_window(root, mycursor):
-    order_window = Toplevel(root)
-    order_window.title('order Data')
-    order_window.geometry('800x550')
+def open_storelocation_window(root, mycursor):
+    storelocation_window = Toplevel(root)
+    storelocation_window.title('storelocation Data')
+    storelocation_window.geometry('800x550')
 
-    order_frame = Frame(order_window, bg='white', relief=RIDGE)
-    order_frame.pack(fill=BOTH, expand=1)
+    storelocation_frame = Frame(storelocation_window, bg='white', relief=RIDGE)
+    storelocation_frame.pack(fill=BOTH, expand=1)
 
-    order_scroll_x = Scrollbar(order_frame, orient=HORIZONTAL)
-    order_scroll_y = Scrollbar(order_frame, orient=VERTICAL)
+    storelocation_scroll_x = Scrollbar(storelocation_frame, orient=HORIZONTAL)
+    storelocation_scroll_y = Scrollbar(storelocation_frame, orient=VERTICAL)
 
-    order_table = ttk.Treeview(order_frame, columns=(
-        'OrderID', 'CustomerID', 'OrderDate', 'TotalAmount', 'PaymentMethod', 'PaymentDate'
+    storelocation_table = ttk.Treeview(storelocation_frame, columns=(
+        'StoreID' ,'StoreName'  , 'Address', 'Phone'  ,  'Manager' , 'OpeningDate'
     ),
-        xscrollcommand=order_scroll_x.set,
-        yscrollcommand=order_scroll_y.set)
+        xscrollcommand=storelocation_scroll_x.set,
+        yscrollcommand=storelocation_scroll_y.set)
 
-    order_scroll_x.config(command=order_table.xview)
-    order_scroll_y.config(command=order_table.yview)
+    storelocation_scroll_x.config(command=storelocation_table.xview)
+    storelocation_scroll_y.config(command=storelocation_table.yview)
 
-    order_scroll_x.pack(side=BOTTOM, fill=X)
-    order_scroll_y.pack(side=RIGHT, fill=Y)
+    storelocation_scroll_x.pack(side=BOTTOM, fill=X)
+    storelocation_scroll_y.pack(side=RIGHT, fill=Y)
 
-    order_table.pack(fill=BOTH, expand=1)
+    storelocation_table.pack(fill=BOTH, expand=1)
 
-    order_table.heading('OrderID', text='OrderID')
-    order_table.heading('CustomerID', text='CustomerID')
-    order_table.heading('OrderDate', text='OrderDate')
-    order_table.heading('TotalAmount', text='TotalAmount')
-    order_table.heading('PaymentMethod', text='PaymentMethod')
-    order_table.heading('PaymentDate', text='PaymentDate')
+    storelocation_table.heading('StoreID', text='StoreID')
+    storelocation_table.heading('StoreName', text='StoreName')
+    storelocation_table.heading('Address', text='Address')
+    storelocation_table.heading('Phone', text='Phone')
+    storelocation_table.heading('Manager', text='Manager')
+    storelocation_table.heading('OpeningDate', text='OpeningDate')
 
-    order_table.column('OrderID', width=20, anchor=CENTER)
-    order_table.column('CustomerID', width=60, anchor=CENTER)
-    order_table.column('OrderDate', width=60, anchor=CENTER)
-    order_table.column('TotalAmount', width=60, anchor=CENTER)
-    order_table.column('PaymentMethod', width=60, anchor=CENTER)
-    order_table.column('PaymentDate', width=60, anchor=CENTER)
+    storelocation_table.column('StoreID', width=20, anchor=CENTER)
+    storelocation_table.column('StoreName', width=60, anchor=CENTER)
+    storelocation_table.column('Address', width=60, anchor=CENTER)
+    storelocation_table.column('Phone', width=60, anchor=CENTER)
+    storelocation_table.column('Manager', width=60, anchor=CENTER)
+    storelocation_table.column('OpeningDate', width=60, anchor=CENTER)
 
     style = ttk.Style()
     style.configure('Treeview', rowheight=40, font=('Helvetica', 10), foreground='black', background='white', fieldbackground='white')
     style.configure('Treeview.Heading', font=('Helvetica', 12, 'bold'), foreground='black', background='light green', fieldbackground='grey')
-    order_table.config(show='headings')
+    storelocation_table.config(show='headings')
 
-    query='SELECT * FROM orders'
+    query='SELECT * FROM storelocations'
     mycursor.execute(query)
     fetched_data=mycursor.fetchall()
 
     for data in fetched_data:
         data_list=list(data)
-        order_table.insert('',END,values=data_list)
+        storelocation_table.insert('',END,values=data_list)
